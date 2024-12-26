883cc92cfdaed04b75ae03987def59808c0b104b
Romain <gerarromain@gmail.com>
2024-12-26 02:43:25 +0100

    Amélioration scripts bash
    Co-authored-by: NicolasVero <nicolasvero03@gmail.com>

--- bash/deploy_to_website.sh
+++ bash/deploy_to_website.sh
@@ -3,36 +3,8 @@
 # Mode strict, arrête le script en cas d'erreur
 set -euo pipefail
 
-CYAN="\033[1;36m"
-RED="\033[1;31m"
-ORANGE="\033[1;33m"
-GREEN="\033[1;32m"
-BLANK_SPACE="\033[0m"
-CARRIAGE_RETURN="\n"
-
 MASTER_BRANCH="master"
 DEPLOY_BRANCH="deploy"
-
-handle_error() {
-  echo -e "${RED}❌ An error occured: $1 (╯°□°)╯︵ ┻━┻ ${BLANK_SPACE}"
-  exit 1
-}
-
-warning_message() {
-  echo -e "${ORANGE}$1 ¯\_(ツ)_/¯ ${BLANK_SPACE}"
-}
-
-success_message() {
-  echo -e "${GREEN}$1 *\(^o^)/* ${BLANK_SPACE}"
-}
-
-info_message() {
-  echo -e "${CYAN}$1${BLANK_SPACE}"
-}
-
-carriage_return_message() {
-	echo -e "${CARRIAGE_RETURN}"
-}
 
 info_message "Fetching the latest changes from origin..."
 git fetch origin "$DEPLOY_BRANCH" || handle_error "Can't fetch '$DEPLOY_BRANCH' branch from origin."
@@ -63,10 +35,10 @@ push_output=$(git push origin "$DEPLOY_BRANCH" 2>&1) || handle_error "Can't push
 carriage_return_message
 
 if echo "$push_output" | grep -q "Everything up-to-date"; then
-  warning_message "The repository is already up-to-date."
+  	warning_message "The repository is already up-to-date."
 else
-  success_message "✅ Changes have been pushed successfully!"
-  success_message "Deployment launched!"
+  	success_message "✅ Changes have been pushed successfully!"
+  	success_message "Deployment launched!"
 fi
 carriage_return_message
 
