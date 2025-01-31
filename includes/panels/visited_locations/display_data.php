ce95ef88bac792ae436115e389c34146d02c477a
BreadyBred <gerarromain@gmail.com>
2025-01-31 12:16:01 +0100

    Junimo kart + Visited Locations panels enhancements

--- includes/panels/visited_locations/display_data.php
+++ includes/panels/visited_locations/display_data.php
@@ -8,22 +8,34 @@ function display_visited_locations_panel(): string {
     $player_id = get_current_player_id();
     $visited_locations = get_locations_visited_data();
     $images_path = get_images_folder();
-    $json_data = $GLOBALS["json"]["locations_to_visit"];
+    $json_data = sanitize_json_with_version("locations_to_visit");
 
-    $locations = "";
-    foreach($json_data as $key => $json_version) {
-        foreach($json_version as $json_line_name) {
-            $is_found = array_key_exists($json_line_name, $visited_locations);
-            $element_class = ($is_found) ? "found" : "not-found";
+    $locations = "";  
+    $batch_size = round(count($json_data) / 2);
+    $counter = 0;
+    
+    foreach($json_data as $json_line_name) {
+        if($counter % $batch_size === 0) {
+            $locations .= "<span class='locations-batch'>";
+        }
 
-            $wiki_link = get_wiki_link(get_item_id_by_name($json_line_name));
-            $locations .= "
-                <a href='$wiki_link' class='wiki_link' rel='noreferrer' target='_blank'>
-                    <span class='$element_class'>" . __($json_line_name) . "</span>
-                </a>
-            ";
+        $is_found = array_key_exists($json_line_name, $visited_locations);
+        $element_class = $is_found ? "found" : "not-found";
+    
+        $wiki_link = get_wiki_link(get_item_id_by_name($json_line_name));
+        $locations .= "
+            <a href='$wiki_link' class='wiki_link' rel='noreferrer' target='_blank'>
+                <span class='$element_class'>" . __($json_line_name) . "</span>
+            </a>
+        ";
+
+        $counter++;
+
+        if($counter % $batch_size === 0 || $counter === count($json_data)) {
+            $locations .= "</span>";
         }
     }
+    
 
     return "
         <section class='visited-locations-$player_id panel visited-locations-panel modal-window'>
