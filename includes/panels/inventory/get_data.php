755b5f9452d635e3edbc4920262f001176487d97
BreadyBred <gerarromain@gmail.com>
2025-03-04 17:51:00 +0100

    Refactoring functions

--- includes/panels/inventory/get_data.php
+++ includes/panels/inventory/get_data.php
@@ -1,35 +1,60 @@
 <?php
 
+/**
+ * Récupère les outils du joueur uniquement si la partie est en singleplayer.
+ * 
+ * @return array Les outils du joueur.
+ */
 function get_player_tools(): array {
 	if(!is_game_singleplayer()) {
 		return [];
 	}
-	# 1.6.0+ only
-	# locations.GameLocation.buildings.Building.buildingChests.Chest.items
-
-	# All versions
-	# locations.GameLocation.objects.item.value.Object.items
-	# locations.GameLocation.fridge.items
-	# locations.GameLocation.buildings.Building.indoors.fridge.items
-	# player.items
-
-	# locations.GameLocation.buildings.Building.indoors.objects.item.value.Object.items
 
 	$data = $GLOBALS["untreated_all_players_data"];
 	$name_object = (is_game_older_than_1_6()) ? "Name" : "name";
-	$player_items = [
-		"Pickaxe" => "Pickaxe",
-		"Axe" => "Axe",
-		"Hoe" => "Hoe",
-		"Can" => "Watering Can",
-		"Pan" => "None",
-		"Rod" => "None",
-		"Scythe" => "Scythe"
+	$player_items = get_starting_tools();
+	$tools_name_dictionary = get_tools_dictionary();
+
+	$player_items_locations = [
+		"player.items",
+		"locations.GameLocation.objects.item.value.Object.items",
+		"locations.GameLocation.buildings.Building.indoors.objects.item.value.Object.items",
+		"locations.GameLocation.buildings.Building.buildingChests.Chest.items",
+		"locations.GameLocation.fridge.items",
+		"locations.GameLocation.buildings.Building.indoors.fridge.items",
 	];
 
-	# Check each item's 'Name' (< 1.6.0) || 'name' (> 1.6.0) to find the tool's name and level
-	# /!\ For: 'Fishing Rods' and 'Scythes', player can have the lower level items
-	$tool_names_dictionnary = [
+	foreach($player_items_locations as $location) {
+		$items_arrays = find_xml_tags($data, $location);
+
+		foreach($items_arrays as $items_array) {
+			foreach($items_array as $item) {
+				$item_name = (string) $item->$name_object;
+				$last_item_word = explode(" ", $item_name);
+				$tool_category = end($last_item_word);
+				$tool_list = $tools_name_dictionary[$tool_category];
+
+				if(!isset($tool_list)) {
+					continue;
+				}
+
+				if(array_search($item_name, $tool_list) > array_search($player_items[$tool_category], $tool_list)) {
+					$player_items[$tool_category] = $item_name;
+				}
+			}
+		}
+	}
+
+	return $player_items;
+}
+
+/**
+ * Récupère le dictionnaire des noms d'outils.
+ * 
+ * @return array Les noms d'outils.
+ */
+function get_tools_dictionary(): array {
+	return [
 		"Pickaxe" => [
 			"Pickaxe",
 			"Copper Pickaxe",
@@ -79,36 +104,21 @@ function get_player_tools(): array {
 			"Iridium Scythe"
 		]
 	];
+}
 
-	$player_items_locations = [
-		"player.items",
-		"locations.GameLocation.objects.item.value.Object.items",
-		"locations.GameLocation.buildings.Building.indoors.objects.item.value.Object.items",
-		"locations.GameLocation.buildings.Building.buildingChests.Chest.items",
-		"locations.GameLocation.fridge.items",
-		"locations.GameLocation.buildings.Building.indoors.fridge.items",
+/**
+ * Récupère les outils de départ d'une partie.
+ * 
+ * @return array Les noms d'outils.
+ */
+function get_starting_tools(): array {
+	return [
+		"Pickaxe" => "Pickaxe",
+		"Axe" => "Axe",
+		"Hoe" => "Hoe",
+		"Can" => "Watering Can",
+		"Pan" => "None",
+		"Rod" => "None",
+		"Scythe" => "Scythe"
 	];
-
-	foreach($player_items_locations as $location) {
-		$items_arrays = find_xml_tags($data, $location);
-
-		foreach($items_arrays as $items_array) {
-			foreach($items_array as $item) {
-				$item_name = (string) $item->$name_object;
-				$last_item_word = explode(" ", $item_name);
-				$tool_category = end($last_item_word);
-
-				if(isset($tool_names_dictionnary[$tool_category])) {
-					$tool_list = $tool_names_dictionnary[$tool_category];
-
-					if(!isset($player_items[$tool_category]) || array_search($item_name, $tool_list) > array_search($player_items[$tool_category], $tool_list)) {
-						$player_items[$tool_category] = $item_name;
-					}
-				}
-			}
-		}
-	}
-	
-	log_($player_items);
-	return [];
 }
