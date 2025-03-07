c3c2cc2b224210372cd4c509a8632813af7aa1df
BreadyBred <gerarromain@gmail.com>
2025-03-07 22:52:43 +0100

    add icon

--- includes/panels/inventory/display_data.php
+++ includes/panels/inventory/display_data.php
@@ -1,6 +1,15 @@
 <?php
 
 /**
+ * Génère le code HTML pour afficher le bouton des outils.
+ *
+ * @return string Le code HTML du bouton des outils.
+ */
+function display_player_tools_button(): string {
+	return "<img src='" . get_images_folder() . "/icons/tools.png' class='tools-icon view-tools view-tools-" . get_current_player_id() . " button-elements modal-opener icon' alt='Tools icon'/>";
+}
+
+/**
  * Génère le code HTML du panneau des outils du joueur.
  * 
  * @return string Le code HTML du panneau des outils du joueur.
@@ -21,13 +30,13 @@ function display_player_tools(): string {
 			<span class='tool'>
                 <span class='tool-category'>$formatted_category</span>
 				<img src='$images_path/tools/$formatted_name.png' class='tool-icon' alt='$player_tool'/>
-				<span class='tool-name'>$player_tool</span>
+				<span class='tool-name'> " . __($player_tool) . "</span>
 			</span>
 		";
     }
 
     return "
-        <section class='tools-$player_id panel tools-panel modal-window'>
+        <section class='tools-$player_id panel tools modal-window'>
             <span class='panel-header'>
                 <h2 class='section-title panel-title'>" . __("Tools") . "</h2>
                 <img src='$images_path/icons/exit.png' class='exit-all-quests-$player_id exit' alt='Exit'/>
