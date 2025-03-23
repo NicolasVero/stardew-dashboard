729358b69b6e6e95b238969530478d9b7fdfde44
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2025-03-23 22:45:53 +0100

    ajout du typage TS
    Co-authored-by: BreadyBred <gerarromain@gmail.com>

--- scripts/typescript/galleries_order.ts
+++ scripts/typescript/galleries_order.ts
@@ -1,7 +1,7 @@
-const gallery_option = document.querySelector(".settings-panel .gallery-order");
+const gallery_option: Element = document.querySelector(".settings-panel .gallery-order");
 
 gallery_option.addEventListener("change", (event) => {
-    const target = event.target as HTMLSelectElement;
+    const target: HTMLSelectElement = event.target as HTMLSelectElement;
 
     if(target.value === "alphabetical-order") {
         gallery_alphabetic_order_display();
@@ -19,10 +19,10 @@ gallery_option.addEventListener("change", (event) => {
 });
 
 function gallery_reset_order() {
-    const galleries_items_containers = document.querySelectorAll(".gallery-items-container");
+    const galleries_items_containers: NodeListOf<Element> = document.querySelectorAll(".gallery-items-container");
 
     galleries_items_containers.forEach((container) => {
-        const tooltips_list = container.querySelectorAll(".tooltip");
+        const tooltips_list: NodeListOf<Element> = container.querySelectorAll(".tooltip");
 
         tooltips_list.forEach((item: HTMLElement) => {
             item.style.removeProperty("order");
@@ -31,28 +31,28 @@ function gallery_reset_order() {
 }
 
 function gallery_alphabetic_order_display() {
-    const galleries_items_containers = document.querySelectorAll(".gallery-items-container");
+    const galleries_items_containers: NodeListOf<Element> = document.querySelectorAll(".gallery-items-container");
 
     galleries_items_containers.forEach((container) => {
-        const tooltips_list = container.querySelectorAll(".tooltip");
-        const items_list = container.querySelectorAll(".tooltip img.gallery-item");
-        const items_names = Array.from(items_list).map((element) => element.getAttribute("alt")).sort();
+        const tooltips_list: NodeListOf<Element> = container.querySelectorAll(".tooltip");
+        const items_list: NodeListOf<Element> = container.querySelectorAll(".tooltip img.gallery-item");
+        const items_names: string[] = Array.from(items_list).map((element) => element.getAttribute("alt")).sort();
     
         tooltips_list.forEach((item: HTMLElement) => {
-            const image_alt = item.querySelector("img.gallery-item").getAttribute("alt");
+            const image_alt: string = item.querySelector("img.gallery-item").getAttribute("alt");
             item.style.order = items_names.indexOf(image_alt).toString();
         });
     });
 }
 
 function gallery_discovery_order_display() {
-    const galleries_items_containers = document.querySelectorAll(".gallery-items-container");
+    const galleries_items_containers: NodeListOf<Element> = document.querySelectorAll(".gallery-items-container");
 
     galleries_items_containers.forEach((container) => {
-        const tooltips_list = container.querySelectorAll(".tooltip");
+        const tooltips_list: NodeListOf<Element> = container.querySelectorAll(".tooltip");
         
         tooltips_list.forEach((item: HTMLElement) => {
-            const item_state = item.querySelector("img.gallery-item").className;
+            const item_state: string = item.querySelector("img.gallery-item").className;
 
             if(item_state.includes("found")) {
                 item.style.order = "0";
